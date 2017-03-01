6d162858a71ae2dfbaa0fdd8a763906182ef0e5b
Ernst-Jan van Woerden <seriousness@Ernst-Jans-MacBook-Pro.local>
2017-03-01 17:41:02 +0800

    disable smooch conversation script

--- script.js
+++ script.js
@@ -8,30 +8,30 @@ module.exports = new Script({
         receive: () => 'processing'
     },
 
-    start: {
-        receive: (bot) => {
-            return bot.say('Hi! I\'m Smooch Bot!')
-                .then(() => 'askName');
-        }
-    },
-
-    askName: {
-        prompt: (bot) => bot.say('What\'s your name?'),
-        receive: (bot, message) => {
-            const name = message.text;
-            return bot.setProp('name', name)
-                .then(() => bot.say(`Great! I'll call you ${name}
-Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
-                .then(() => 'finish');
-        }
-    },
-
-    finish: {
-        receive: (bot, message) => {
-            return bot.getProp('name')
-                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
-                        'teach me how to do anything else!'))
-                .then(() => 'finish');
-        }
-    }
+//    start: {
+//        receive: (bot) => {
+//            return bot.say('Hi! I\'m Serious Bot!')
+//                .then(() => 'askName');
+//        }
+//    },
+//
+//    askName: {
+//        prompt: (bot) => bot.say('What\'s your name fool?'),
+//        receive: (bot, message) => {
+//            const name = message.text;
+//            return bot.setProp('name', name)
+//                .then(() => bot.say(`Great! I'll call you ${name}
+//Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
+//                .then(() => 'finish');
+//        }
+//    },
+//
+//    finish: {
+//        receive: (bot, message) => {
+//            return bot.getProp('name')
+//                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
+//                        'teach me how to do anything else, better go away. BYE!'))
+//                .then(() => 'finish');
+//        }
+//    }
 });
